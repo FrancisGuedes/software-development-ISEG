38dc1fea5599421bc9d815c4cac9eb7eb8eacb15
Francis Guedes <62439681+FrancisGuedes@users.noreply.github.com>
2021-02-20 15:16:13 +0000

    Create work1.py

---
+++ work1.py
@@ -0,0 +1,31 @@
+## Problema 1
+
+Pretende-se desenvolver um programa que obtenha um texto a partir da linha de comandos e
+converta esse texto da seguinte forma:
+
+• O texto deve ser ordenado de forma inversa, i.e., se a entrada for “ola” o sistema converte
+para “alo”
+• As vogais devem ser convertidas para maiúsculas (se forem minúsculas) ou para minúsculas
+(se forem maiúsculas)
+
+Exemplos de testes:
+➢ Introduza um texto: Um bom dia a todos!
+A aplicação deve escrever na consola:
+➢ !sOdOt A AId mOb mu
+
+#### NOTA: não é permitido usar funções e extensões de Python, como replace(). Devem limitar-se à
+utilização dos comandos aprendidos em aula.
+
+## Problema 2
+
+Pretende-se desenvolver um programa de radiciação, i.e, para o cálculo de raízes , considerando
+que a solução é o valor V, se Vn = a
+
+O programa deve receber como parâmetros os valores de n e de a, imprimindo o valor de V como
+resultado. Adicionalmente, deve ter um parâmetro para escolher o algoritmo de implementação,
+sendo possível optar por G (algoritmo de Guess and Check), A (algoritmo de soluções aproximadas) e
+B (algoritmos de bissecção).
+
+Devem seguir o exemplo das aulas (que ilustra a implementação dos três algoritmos para a raiz cúbica)
+e adaptá-lo para que funcione com quaisquer valores e imprima também o número de iterações
+necessárias para chegar ao resultado.
